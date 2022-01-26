02bb1541cdfb1b9e4fb2348e630c9c8261d2973e
user211270 <user211270@FF075.local>
2022-01-26 17:15:47 +0100

    Initial commit, 1 point

---
+++ ifkadyrovPW7/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ifkadyrovPW7
+//
+//  Created by user211270 on 1/26/22.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
